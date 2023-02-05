abe0562a2cd1285ae8d23c338a44e8340591e537
Roshan panda <roshanpanda666@gmail.com>
2023-02-05 18:45:31 +0530

    commiting for the first time on this repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenceItem from "./components/Component1"
+
+function App(){
+    return(
+        <div>
+            <h2>lets get started</h2>
+            <p>this is roshan</p>
+            <ExpenceItem></ExpenceItem>
+        </div>
+
+    )
+
+}
+
+export default App
